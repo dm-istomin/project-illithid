1e4b0682dc0c7d59b520f35c62b21b2eb6bcc954
Griffin Smith <wildgriffin45@gmail.com>
2017-01-21 18:03:04 -0500

    Make the tests for push-raw more explicit

--- test/illithid/handlers/nav_test.cljs
+++ test/illithid/handlers/nav_test.cljs
@@ -1,13 +1,32 @@
 (ns illithid.handlers.nav-test
   (:require [cljs.test :refer-macros [deftest is testing]]
             [clojure.spec :as s]
-            [illithid.test-helpers.spec-check :refer-macros [check]]
+            [clojure.test.check.clojure-test :refer-macros [defspec]]
+            [clojure.test.check.properties :as prop :include-macros true]
+            [clojure.test.check.generators :as gen]
+            [clojure.spec.test :as stest]
+            [illithid.db :as db]
             [illithid.handlers.nav :refer [push-raw]]))
 
-(deftest push-raw-test
-  (s/exercise-fn push-raw)
-  (check
-    `push-raw
-    {:gen {:illithid.db/state  #(s/gen #{:illithid.db/home})
-           :illithid.db/app-db #(s/gen (s/keys :req [:illithid.db/nav
-                                                     :illithid.db/state]))}}))
+(stest/instrument)
+
+(defspec new-route-adds-to-end 5
+  (prop/for-all
+    [[db route] (gen/such-that
+                  #(every? (complement #{%2}) (-> %1 ::db/nav :routes))
+                  (gen/tuple (s/gen ::db/app-db)
+                             (s/gen ::db/route)))]
+    (let [result (push-raw db [:nav/push-raw route])]
+      (= route (-> result ::db/nav :routes last)))))
+
+(defspec duplicate-route-adds-to-end 5
+  (prop/for-all
+    [[db route] (gen/bind
+                  (gen/such-that #(-> % ::db/nav :routes seq)
+                                 (s/gen ::db/app-db))
+                  (fn [db]
+                    (gen/tuple (gen/return db)
+                               (-> db ::db/nav :routes gen/elements))))]
+    (let [result (push-raw db [:nav/push-raw route])]
+      (= route (-> result ::db/nav :routes last)))))
+
