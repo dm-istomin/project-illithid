fa93f2a4ccc600a6fce4cdb165382d976bd2bd82
Griffin Smith <wildgriffin45@gmail.com>
2017-01-28 15:17:40 -0500

    Splice the list of spells using a macro
    Splice the list of spells into the `illithid.character.spells` namespace
    using a macro, rather than having them all hardcoded

--- src/illithid/resources.clj
+++ src/illithid/resources.clj
@@ -1,13 +1,31 @@
 (ns illithid.resources
-  (:require [clojure.java.io :refer [resource file]]
+  (:require [clojure.java.io :refer [reader resource file]]
             [clojure.edn :as edn]
+            [clojure.string :refer [split]]
             [clojure.spec :as s])
-  (:import [java.io FileReader PushbackReader]))
+  (:import [java.io File PushbackReader]))
 
 (defn read-resource [filename]
-  (-> filename resource file FileReader. PushbackReader. edn/read))
+  (-> filename resource file reader PushbackReader. edn/read))
+
+(defn read-resources [dir] ;; -> [{:name filename, :body edn-file-body}, ...]
+  (for [^File f (->> dir resource file .listFiles)]
+    (with-open [rd (-> f reader PushbackReader.)]
+      {:name (.getName f)
+       :body (edn/read rd)})))
+
+(defn resource-map [dir & {:keys [spec]}];;->{:file-name-1 edn-file-body-1, ...}
+  (into
+    {}
+    (for [{filename :name, body :body} (read-resources dir)]
+      (let [fname (-> filename (split #"\.") first)]
+        (when spec (s/assert* spec body))
+        [(keyword fname) body]))))
 
 (defmacro splice-resource [filename] (read-resource filename))
+
+(defmacro defresources [dir & {:keys [spec]}]
+  `(def ~dir ~(-> dir name (resource-map :spec spec))))
 
 (defmacro resource-macro [n & {:keys [folder spec] :or {folder (str n "s")}}]
   (let [assert-form (fn [body] (if spec `(s/assert* ~spec ~body) body))
