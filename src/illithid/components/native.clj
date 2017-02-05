4ce6c4f4d9a9ca6702d6b4552d337f5be147bd67
Griffin Smith <wildgriffin45@gmail.com>
2017-02-05 11:14:18 -0500

    Update defclass[es] to handle nested components
    Update the defclass and, by extension, the defclasses macros to handle
    components that are defined not just as direct properties of
    react-native, but also nested properties. The syntax is a vector of
    symbols naming the path of attributes in react-native.

--- src/illithid/components/native.clj
+++ src/illithid/components/native.clj
@@ -1,20 +1,54 @@
-(ns illithid.components.native)
+(ns illithid.components.native
+  (:require [clojure.spec :as s]
+            [clojure.core.match :refer [match]]))
+
+(do ;; specs {{{
+  (s/def ::react-class-spec (s/or :react-property simple-symbol?
+                                  :nested-property (s/coll-of simple-symbol?
+                                                              :kind vector?
+                                                              :into []
+                                                              :min-count 1)))
+  (s/def ::defclass-args
+    (s/cat :var-name simple-symbol?
+           :class-name ::react-class-spec))
+
+  (s/def ::defclasses-args (s/cat :classes (s/* ::defclass-args))))
+;; }}}
 
 (defmacro defclass
-  "Define `var-name` as an adapted React class based on js/React.<attr>"
-  [var-name attr]
-  `(def ~var-name
-     (when illithid.react/native?
-       (reagent.core/adapt-react-class
-         (aget illithid.react/react ~(name attr))))))
+  "Define `var-name` as an adapted React class based on js/React.<class-name>
+
+   `class-name` can either be a symbol naming an attribute of js/react-native,
+   or a vector of symbols naming a nested path of attributes in js/react-native"
+  [var-name class-name]
+  (let [property
+        (match [(s/conform ::react-class-spec class-name)]
+          [[:react-property p]]   `(aget illithid.react/react ~(name p))
+          [[:nested-property ps]] `(aget illithid.react/react ~@(map name ps)))]
+    `(def ~var-name
+       (when illithid.react/native?
+         (reagent.core/adapt-react-class ~property)))))
+
+(s/fdef defclass :args ::defclass-args)
 
 (defmacro defclasses
-  "Define pairs of arguments as adapted react classes as per `defclass`"
+  "Define pairs of arguments as adapted react classes as per `defclass`.
+
+   The second half of the pair can either be a symbol naming an attribute of
+   js/react-native, or a vector of symbols naming a nested path of attributes in
+   js/react-native"
   {:arglists '([var-name attr & var-name attr ...])}
   [& args]
   {:pre  [(even? (count args))]
-   :post [(= (+ 1 ;; the +1 is for the `do`
+   :post [(= (+ 2 ;; the +2 are for the `do` and the `nil`
                 (/ (count args) 2)) (count %))]}
-  `(do
-     ~@(for [[var-name attr] (partition 2 args)]
-         `(defclass ~var-name ~attr))))
+  (let [{:keys [classes]} (s/conform ::defclasses-args args)]
+    `(do
+       ~@(for [{:keys [var-name class-name]} classes]
+           `(defclass ~var-name ~(second class-name)))
+       nil)))
+
+(s/fdef defclasses :args ::defclasses-args)
+
+; vim:fdm=marker:fmr={{{,}}}:
+
